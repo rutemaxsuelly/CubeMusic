f57b1835cf18800828f3d3881215135483d4c87e
Rute Maxsuelly <rutymaxsuelly@gmail.com>
2018-10-26 08:02:06 -0300

    Atualização das cenas

---
+++ Unity/CubeMusic/Assets/Scripts/CountDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour {
+	public Text displayContagem;
+
+	public float contagem = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (contagem > 0.0f)
+		{
+			contagem -= Time.deltaTime;
+			displayContagem.text = contagem.ToString("F1");
+		}
+		else
+		{
+			displayContagem.text = "Fim";
+		}
+
+	}
+}
