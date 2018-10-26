263e81b6d79fee63ae09c1a9c873d56740b867a6
Rute Maxsuelly <rutymaxsuelly@gmail.com>
2018-10-26 08:27:49 -0300

    Script conector de telas

---
+++ Unity/CubeMusic/Assets/Scripts/Level_Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Level_Script : MonoBehaviour {
+
+    void Start()
+    {
+     //   Debug.Log("LoadSceneB");
+    }
+
+    public void LoadB(int sceneANumber)
+    {
+        Debug.Log("sceneBuildIndex to load: " + sceneANumber);
+        SceneManager.LoadScene(sceneANumber);
+    }
+
+    public void FecharJogo()
+    {
+        Application.Quit();
+    }
+
+}
