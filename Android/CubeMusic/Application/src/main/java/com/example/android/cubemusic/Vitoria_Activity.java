3d81c1f6f73c758064910e6f135e6f74418aaa71
Rute Maxsuelly <rutymaxsuelly@gmail.com>
2018-11-19 10:41:15 -0300

    Tela de vitória e derrota

---
+++ Android/CubeMusic/Application/src/main/java/com/example/android/cubemusic/Vitoria_Activity.java
@@ -0,0 +1,14 @@
+package com.example.android.cubemusic;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Vitoria_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vitoria_);
+    }
+
+}
